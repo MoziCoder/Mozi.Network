d09ff2a52627ae0b4ff9ded2f17c0ab3770c3b29
Jason <brotherqian@163.com>
2021-11-24 16:24:41 +0800

    增加新的项目规划

---
+++ Mozi.NTP/UDPStateObject.cs
@@ -0,0 +1,13 @@
+﻿using System.Net;
+using Mozi.NTP;
+
+namespace Mozi.SSDP
+{
+    /// <summary>
+    /// UDP通讯缓冲对象
+    /// </summary>
+    public class UDPStateObject : StateObject
+    {
+        public EndPoint RemoteEndPoint;
+    }
+}
