2d705bfac24df22e36ce7092234f94d3aacf97da
Jason <brotherqian@163.com>
2022-05-06 09:16:49 +0800

    提升版本号

---
+++ Mozi.HttpEmbedded/ContentRange.cs
@@ -0,0 +1,13 @@
+﻿namespace Mozi.HttpEmbedded
+{
+    /// <summary>
+    /// 请求内容的范围
+    /// </summary>
+    public class ContentRange
+    {
+        //数据为uint 这里为了表示更大范围，用Int64表示
+        public long Start { get; set; }
+
+        public long End { get; set; }
+    }
+}
--- Mozi.HttpEmbedded/HttpServer.cs
+++ Mozi.HttpEmbedded/HttpServer.cs
@@ -1008,15 +1008,4 @@ namespace Mozi.HttpEmbedded
     }
 
     public delegate void Request(string srcHost, int srcPort, HttpRequest request);
-
-    /// <summary>
-    /// 请求内容的范围
-    /// </summary>
-    public class ContentRange
-    {
-        //数据为uint 这里为了表示更大范围，用Int64表示
-        public long Start { get; set; }
-
-        public long End { get; set; }
-    }
 }
