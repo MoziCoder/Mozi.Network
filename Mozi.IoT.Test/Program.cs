f3acad7f94957d70b521f1808ab473c327492e87
Jason <brotherqian@163.com>
2022-05-31 09:02:15 +0800

    提升版本号

--- Mozi.IoT.Test/Program.cs
+++ Mozi.IoT.Test/Program.cs
@@ -1,48 +1,66 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
 
 namespace Mozi.IoT.Test
 {
     class Program
     {
+        
+        private static ulong _packetReceived=0;
+        private static ulong _bytesReceived = 0;
+        private static ulong pps = 0;
         static void Main(string[] args)
         {
-            //服务端
-            CoAPServer cs = new CoAPServer();
-            cs.RequestReceived += new MessageTransmit((host, port, pack) =>
+            //Stopwatch sp = new Stopwatch();
+            //////服务端
+            //CoAPServer cs = new CoAPServer();
+            //cs.RequestReceived += new MessageTransmit((host, port, pack) =>
+            //{
+            //    //Console.WriteLine($"Rev count:{PacketReceivedCount},current:{args.Data.Length}bytes,total:{TotalReceivedBytes}bytes");
+            //    Console.WriteLine($"From:[{host}:{port}]");
+            //    Console.WriteLine(pack.ToString());
+            //    Console.Title = string.Format("elapsed:{2},count:{0},pps:{3},bytes:{1}", cs.PacketReceivedCount, cs.TotalReceivedBytes,FormatSeconds(sp.ElapsedMilliseconds),pps);
+            //});
+
+            //Timer timer = new Timer((x) =>
+            //{
+            //    pps = cs.PacketReceivedCount - _packetReceived;
+
+            //    _packetReceived = cs.PacketReceivedCount;
+            //    _bytesReceived = cs.TotalReceivedBytes;
+            //});
+            //timer.Change(0, 1000);
+            //sp.Start();
+            //cs.Start();
+            //Console.ReadLine();
+
+            //客户端 多线程并发
+            List<CoAPClient> ccs = new List<CoAPClient>();
+            for (int i = 0; i < 4; i++)
             {
-                //Console.WriteLine($"Rev count:{PacketReceivedCount},current:{args.Data.Length}bytes,total:{TotalReceivedBytes}bytes");
-                Console.WriteLine($"From:[{host}:{port}]");
-                Console.WriteLine(pack.ToString());
-                Console.Title = String.Format("count:{0},bytes:{1}", cs.PacketReceivedCount, cs.TotalReceivedBytes);
-            });
-            cs.Start();
-            Console.ReadLine();
-
-            ////客户端 多线程并发
-            //List<CoAPClient> ccs = new List<CoAPClient>();
-            //for (int i = 0; i < 32; i++)
-            //{
-            //    CoAPClient cc = new CoAPClient();
-            //    //本地端口
-            //    cc.SetPort(10 + i);
-            //    cc.Start();
-            //    ccs.Add(cc);
-            //}
-            ////电脑主机如果性能不行，不要尝试下面的方法
-            //foreach (CoAPClient cc in ccs)
-            //{
-
-            //    var td = (new Thread(x =>
-            //    {
-            //        for (int i = 0; i < 1000000; i++)
-            //        {
-            //            //cc.Get("coap://127.0.0.1/sensor/test/sensor/test/sensor/test/sensor/test/sensor/test/sensor/test/sensor/test/sensor/test/sensor/test/sensor/test/sensor/test/sensor/test");
-            //            cc.Get("coap://127.0.0.1/sensor", CoAPMessageType.Confirmable);
-            //        }
-            //    }));
-            //    td.Priority = ThreadPriority.Highest;
-            //    td.Start();
-            //}
+                CoAPClient cc = new CoAPClient();
+                //本地端口
+                cc.SetPort(10 + i);
+                cc.Start();
+                ccs.Add(cc);
+            }
+            //电脑主机如果性能不行，不要尝试下面的方法
+            foreach (CoAPClient cc in ccs)
+            {
+                var td = (new Thread(x =>
+                {
+                    for (int i = 0; i < 1000000; i++)
+                    {
+                        //cc.Get("coap://127.0.0.1/sensor/test/sensor/test/sensor/test/sensor/test/sensor/test/sensor/test/sensor/test/sensor/test/sensor/test/sensor/test/sensor/test/sensor/test");
+                        cc.Get("coap://127.0.0.1/sensor", CoAPMessageType.Confirmable);
+                        Thread.Sleep(1);
+                    }
+                }));
+                td.Priority = ThreadPriority.Highest;
+                td.Start();
+            }
 
             ////客户端调用 基本
             //CoAPClient cc = new CoAPClient();
@@ -63,6 +81,16 @@ namespace Mozi.IoT.Test
 
             Console.ReadLine();
 
+        }
+
+        private static string FormatSeconds(long ms)
+        {
+            var seconds = ms / 1000;
+            var day = seconds / 3600 / 24;
+            var hour = (seconds - (day * 3600 * 24)) / 3600;
+            var minute = (seconds - (day * 3600 * 24) - hour * 3600) / 60;
+            var second = seconds - day * 3600 * 24 - hour * 3600 - minute * 60;
+            return string.Format("{0}天,{1:00}:{2:00}:{3:00}", day, hour, minute, second);
         }
     }
 }
